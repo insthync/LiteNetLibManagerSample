c66cdceb17fafbf1184bf4de46575842c2388d8a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-25 23:06:19 +0700

    Test networking spawn/destroy

---
+++ LiteNetLibManagerSample/Assets/Demo/Scripts/UITestSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITestSpawn : MonoBehaviour
+{
+    public InputField spawnX;
+    public InputField spawnY;
+    public InputField spawnZ;
+
+    public void OnClickSpawn()
+    {
+        var prefab = CustomNetworkManager.Singleton.Assets.registeringPrefabs[0].gameObject;
+        CustomNetworkManager.Singleton.Assets.NetworkSpawn(prefab, new Vector3(float.Parse(spawnX.text), float.Parse(spawnY.text), float.Parse(spawnZ.text)));
+    }
+}
