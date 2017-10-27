291f61c27933f2a2713d8b692d45e0451e285d07
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-27 08:39:27 +0700

    Update demo

--- LiteNetLibManagerSample/Assets/Demo/Scripts/CustomNetworkBehaviour.cs
+++ LiteNetLibManagerSample/Assets/Demo/Scripts/CustomNetworkBehaviour.cs
@@ -11,9 +11,9 @@ public class CustomNetworkBehaviour : LiteNetLibBehaviour
 
     private void Awake()
     {
-        RegisterSyncField(1, test1);
-        RegisterSyncField(2, test2);
-        RegisterSyncField(3, test3);
+        RegisterSyncField("test1", test1);
+        RegisterSyncField("test2", test2);
+        RegisterSyncField("test3", test3);
         var syncFieldUI = FindObjectOfType<UITestSyncFields>();
         syncFieldUI.behaviour = this;
     }
