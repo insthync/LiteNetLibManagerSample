cf9a29153e06cfeae932a81fbb5b87e9645424b8
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-19 04:15:39 +0700

    Add sync transform demo

---
+++ LiteNetLibManagerSample/Assets/Demo/Scripts/SimpleCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLibHighLevel;
+
+public class SimpleCharacterController : LiteNetLibBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsLocalClient)
+            transform.Translate(Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), 1) * 2 * Time.fixedDeltaTime);
+    }
+}
