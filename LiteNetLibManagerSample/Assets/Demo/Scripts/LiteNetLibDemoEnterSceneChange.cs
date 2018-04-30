eb192b12511fcf0a92f87bc75d2887260d349afe
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-04-30 18:11:23 +0700

    Add scene change demo

---
+++ LiteNetLibManagerSample/Assets/Demo/Scripts/LiteNetLibDemoEnterSceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLibHighLevel;
+
+public class LiteNetLibDemoEnterSceneChange : MonoBehaviour
+{
+    public LiteNetLibScene scene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var networkManager = FindObjectOfType<LiteNetLibGameManager>();
+        if (networkManager != null && Input.GetKeyDown(KeyCode.F1))
+            networkManager.ServerSceneChange(scene.SceneName);
+    }
+}
